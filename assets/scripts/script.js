1177af5873726cdf0f2ee61fb85c4658e43f0f9a
Jose Rosas <josejrrosas@yahoo.com>
2021-06-09 22:20:43 -0500

    was able to do fetch within fetch to get weather details put into the currentWeather class'

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,10 +11,28 @@ var currentUVIndexEl = document.querySelector('#UVIndex');
 var forecastCardEl = document.getElementsByClassName('forecastCard');
 
 var myKey = 'e55d093b8b969691fea4b7cda7ecf344';
-searchButtonEl.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInputEl.value+'&appid=e55d093b8b969691fea4b7cda7ecf344')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    
-    .catch(err => alert("Wrong city name!"));
-    })
+searchButtonEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchInputEl.value + '&appid=e55d093b8b969691fea4b7cda7ecf344')
+        .then(response => response.json())
+        .then(data => {
+            var lat = data['coord']['lat'];
+            var long = data['coord']['lon']
+            
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&exclude=hourly,daily&appid=e55d093b8b969691fea4b7cda7ecf344')
+                .then(response => response.json())
+                .then(data => {
+                    var tempValue = data['current']['temp'];
+                    var windValue = data['current']['wind_speed'];
+                    var humidityValue = data['current']['humidity'];
+                    var indexValue = data['current']['uvi'];
+
+                    currentTempEl.innerHTML = tempValue;
+                    currentWindEl.innerHTML = windValue;
+                    currentHumidityEl.innerHTML = humidityValue;
+                    currentUVIndexEl.innerHTML = indexValue;
+                })
+        })
+        .catch(err => alert("Wrong city name!"));
+})
+
